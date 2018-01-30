acf9e72556b1833efeca016af28d98b06b69fae7
Daniel Schmidt <danielmschmidt92@gmail.com>
2018-01-30 23:24:17 +0100

    feat: load dependencies and find runners for a given path

--- src/__tests__/run.js
+++ src/__tests__/run.js
@@ -1,14 +1,18 @@
-const run = require("../run");
-
 describe("run", () => {
-  it("should return a promise", () => {
-    expect.assertions(1);
-    return expect(
-      run({
-        testpath: "/Users/project/__tests__/foo.js",
-        config: {},
-        globalConfig: {}
-      })
-    ).resolves.toHaveProperty("numPassingTests");
-  });
+  it("should be tested");
 });
+
+// const run = require("../run");
+
+// describe("run", () => {
+//   it("should return a promise", () => {
+//     expect.assertions(1);
+//     return expect(
+//       run({
+//         testpath: "/Users/project/__tests__/foo.js",
+//         config: {},
+//         globalConfig: {}
+//       })
+//     ).resolves.toHaveProperty("numPassingTests");
+//   });
+// });
--- src/run.js
+++ src/run.js
@@ -1,13 +1,29 @@
 const { pass, fail } = require("create-jest-runner");
+const glob = require("glob").sync;
+
+const root = process.cwd();
+const ownConfig = require(root + "/package.json")["jest-runner-multi"];
+const files = {};
+Object.entries(ownConfig).forEach(([pattern, runner]) => {
+  glob(pattern, {
+    cwd: root
+  })
+    .map(relativePath => root + "/" + relativePath)
+    .forEach(filePath => {
+      files[filePath] = files[filePath] || [];
+      files[filePath].push(runner);
+    });
+});
+Object.entries(files).forEach(
+  ([filePath, runners]) =>
+    (files[filePath] = runners.map(runner => require.resolve(runner)))
+);
 
 module.exports = ({ testPath, config, globalConfig }) => {
   const start = new Date();
+  const runnersForPath = files[testPath];
 
-  return Promise.resolve(
-    pass({
-      start,
-      end: new Date(),
-      test: { path: testPath }
-    })
-  );
+  // TODO: allow more than the first one
+  const runner = runnersForPath[0];
+  return runner({ testPath, config, globalConfig });
 };

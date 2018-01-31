84eebbcb13caed17d4dc10097608b4aa6e67929e
Daniel Schmidt <danielmschmidt92@gmail.com>
2018-01-31 08:09:26 +0100

    refactor(run): move to minimatch
    Closes #3

--- src/run.js
+++ src/run.js
@@ -1,28 +1,32 @@
-const { pass, fail } = require("create-jest-runner");
-const glob = require("glob").sync;
+const { skip } = require("create-jest-runner");
+const minimatch = require("minimatch");
 
 const root = process.cwd();
 const ownConfig = require(root + "/package.json")["jest-runner-multi"];
-const files = {};
-Object.entries(ownConfig).forEach(([pattern, runner]) => {
-  glob(pattern, {
-    cwd: root
-  })
-    .map(relativePath => root + "/" + relativePath)
-    .forEach(filePath => {
-      files[filePath] = files[filePath] || [];
-      files[filePath].push(runner);
-    });
-});
-Object.entries(files).forEach(
-  ([filePath, runners]) =>
-    (files[filePath] = runners.map(runner => require.resolve(runner)))
+
+// Load runner
+const runners = {};
+// Object.values(ownConfig).forEach(runnerName => runners[runnerName] = require.resolve(runnerName));
+Object.values(ownConfig).forEach(
+  runnerName => (runners[runnerName] = runnerName)
 );
 
 module.exports = ({ testPath, config, globalConfig }) => {
   const start = new Date();
-  const runnersForPath = files[testPath];
+  const runnersForPath = Object.entries(ownConfig)
+    .filter(([pattern]) => minimatch(testPath, pattern))
+    .map(([, runnerName]) => runners[runnerName]);
 
+  if (!runnersForPath.length) {
+    // No runner found, so we skip
+    return skip({
+      start,
+      end: new Date(),
+      test: {
+        path: testPath
+      }
+    });
+  }
   // TODO: allow more than the first one
   const runner = runnersForPath[0];
   return runner({ testPath, config, globalConfig });
